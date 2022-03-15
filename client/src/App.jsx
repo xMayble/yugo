34a3e6ca9aaf188541dd17d1675c75de8f648a5c
Alex Ilski <aleksander.ilski@yahoo.com>
2022-03-14 22:57:03 -0400

    changed message

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">
-        Hello world!
+        Hello world!!
       </h1>  
     </div>
   )
