d73f89934cd14217db5767f4873f96f11187dd2d
Mayble <mehburohit@gmail.com>
2022-03-16 03:44:08 -0400

    added all the components in one index folder

---
+++ client/src/elements/index.js
@@ -0,0 +1,7 @@
+export { defaut as Loader } from './Loader';
+export { defaut as Navbar } from './Navbar';
+export { defaut as Footer } from './Footer';
+export { defaut as Services } from './Services';
+export { defaut as Transactions } from './Transactions';
+export { defaut as Welcome } from './Welcome';
+ 
