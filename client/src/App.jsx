d131f5fa1aed327e2190cc6ee965e73fafce6184
Tazrian <tazrianh98@gmail.com>
2022-03-23 17:51:22 -0400

    fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,5 @@
 import { Navbar, Welcome, Footer, Services, Transactions } from './elements';
 import './App.css'
-import Homepage from "./components/homepage/homepage"
-import Login from "./components/login/login"
-import Register from "./components/register/register"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { useEffect, useState } from 'react';
 
 
