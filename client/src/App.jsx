3f2dd9349a0aed9386f260c78ef2179c6d6a6a62
Tazrian <tazrianh98@gmail.com>
2022-03-23 17:13:10 -0400

    login and register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,23 @@
 import { Navbar, Welcome, Footer, Services, Transactions } from './elements';
+import './App.css'
+import Homepage from "./components/homepage/homepage"
+import Login from "./components/login/login"
+import Register from "./components/register/register"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useEffect, useState } from 'react';
+
 
 const App = () => {
+  const [ user, setLoginUser] = useState({})
+
+  useEffect(() => {
+    setLoginUser(JSON.parse(localStorage.getItem("MyUser")))
+  }, [])
+
+  const updateUser = (user) => {
+    localStorage.setItem("MyUser", JSON.stringify(user))
+    setLoginUser(user)
+  }
   return (
     <div className="min-h-screen">
       <div className="gradient-bg-welcome">
