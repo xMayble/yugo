88a0fcfabf8ceb8c6fabc492d0c175c30b5c207c
Adib Rahim <adibrahim27@gmail.com>
2022-05-18 15:20:30 -0400

    duplicate buttons deleted

--- client/src/elements/Navbar.jsx
+++ client/src/elements/Navbar.jsx
@@ -34,12 +34,7 @@ const [toggleMenu, setToggleMenu] = useState(false);
                 <li className='bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]'>
                     <a href=''>Claim</a>
                 </li>
-                <li className='bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]'>
-                    Market
-                </li>
-                <li className='bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]'>
-                    Claim
-                </li>
+               
             </ul>
             <div className='flex relative'>
                 {toggleMenu
