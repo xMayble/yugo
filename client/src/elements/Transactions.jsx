f5a0f0bb9fb43e410dd12a27b4c885dde1a8f10b
Mayble <mehburohit@gmail.com>
2022-05-18 21:42:03 -0400

    almost done w styling of list page

--- client/src/elements/Transactions.jsx
+++ client/src/elements/Transactions.jsx
@@ -10,11 +10,7 @@ const Transactions = () => {
     return (
         <div className="flex w-full justify-center items-center 2x1:px-20 gradient-bg-transactions">
             <div className='flex flex-col md:p-12 py-12 px-4'>
-                {currentAccount ? (
-                    <h3 className='text-white text-3xl text-center my-2'> Latest Transactions </h3>  
-                ) : (
-                    <h3 className='text-white text-3xl text-center my-2'> Connect your account to see the latest transactions </h3>  
-                )}
+         
             </div>
         </div>
     );
