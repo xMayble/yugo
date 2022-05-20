95d265f46de57570ebef93b00495993545e28ccd
Tazrian <tazrianh98@gmail.com>
2022-05-19 22:56:30 -0400

    tweaks

--- client/src/elements/Signin.jsx
+++ client/src/elements/Signin.jsx
@@ -23,6 +23,7 @@ const Signin = () => {
   };
 
   return (
+    <nav className='w-full flex md:justify-center justify-between items-center p-4'>
     <div className='max-w-[700px] mx-auto my-16 p-4'>
       <div>
         <h1 className='text-2xl font-bold py-2'>Sign in to your account</h1>
@@ -42,11 +43,12 @@ const Signin = () => {
           <label className='py-2 font-medium'>Password</label>
           <input onChange={(e) => setPassword(e.target.value)} className='border p-3' type='password' />
         </div>
-        <button className='border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full p-4 my-2 text-white'>
+        <button className='border border-purple-500 bg-purple-600 hover:bg-violet-500 w-full p-4 my-2 text-white'>
           Sign In
         </button>
       </form>
     </div>
+    </nav>
   );
 };
 
--- client/src/elements/Signup.jsx
+++ client/src/elements/Signup.jsx
@@ -26,6 +26,7 @@ const Signup = () => {
   };
 
   return (
+    <nav className='w-full flex md:justify-center justify-between items-center p-4'>
     <div className='max-w-[700px] mx-auto my-16 p-4'>
       <div>
         <h1 className='text-2xl font-bold py-2'>Sign up for a free account</h1>
@@ -46,18 +47,19 @@ const Signup = () => {
           />
         </div>
         <div className='flex flex-col py-2'>
-          <label className='py-2 font-medium'>Password</label>
+          <label className='py-2 font-medium'>Password(Must be at least 6 characters)</label>
           <input
             onChange={(e) => setPassword(e.target.value)}
             className='border p-3'
             type='password'
           />
         </div>
-        <button className='border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full p-4 my-2 text-white'>
+        <button className='border border-purple-500 bg-purple-600 hover:bg-violet-500 w-full p-4 my-2 text-white'>
           Sign Up
         </button>
       </form>
     </div>
+    </nav>
   );
 };
 
