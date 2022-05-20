a4710dd39a42cfae8c31667655085fb0a1f85854
Tazrian <tazrianh98@gmail.com>
2022-05-20 01:51:04 -0400

    account page

--- client/src/elements/Account.jsx
+++ client/src/elements/Account.jsx
@@ -17,14 +17,16 @@ const Account = () => {
   };
 
   return (
-    <div className='max-w-[600px] mx-auto my-16 p-4'>
-      <h1 className='text-2xl font-bold py-4'>Account</h1>
-      <p>User Email: {user && user.email}</p>
+    <section className='hero'>
+      <nav>
+      <h1 className='text-2xl font-bold py-4 text-white'>Account</h1>
+      <p className='text-white'>User Email: {user && user.email}</p>
 
-      <button onClick={handleLogout} className='border px-6 py-2 my-4'>
+      <button onClick={handleLogout} >
         Logout
       </button>
-    </div>
+      </nav>
+    </section>
   );
 };
 
